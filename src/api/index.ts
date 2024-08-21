dbd221097ea83d215e8487dd39c00a096f709e47
BillyDyball <47304846+BillyDyball@users.noreply.github.com>
2024-08-21 22:56:02 +0100

    Roasts working

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,64 @@
 import { AppBskyActorGetProfile, AtpAgent } from "@atproto/api";
+import {
+  EnhancedGenerateContentResponse,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
 
-const agent = new AtpAgent({
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction:
+    "You are a witty assistant asked to create a light-hearted roast.",
+});
+
+const bskyAgent = new AtpAgent({
   service: "https://bsky.social",
 });
 
-type ResponseError = {
-  response: AppBskyActorGetProfile.Response | null;
-  error: string | null;
-};
+/** [Response, Error] */
+type ResponseError<T = unknown> = [T | null, string | null];
+
+type BskyResponse = ResponseError<AppBskyActorGetProfile.Response>;
+type GenResponse = ResponseError<EnhancedGenerateContentResponse>;
 
 export const agentLogin = async () => {
-  console.log(
-    "import.meta.env.VITE_BLUE_SKY_PROFILE",
-    import.meta.env.VITE_BLUE_SKY_PROFILE,
-    import.meta.env.VITE_BLUE_SKY_KEY
-  );
-  await agent.login({
+  await bskyAgent.login({
     identifier: import.meta.env.VITE_BLUE_SKY_PROFILE,
     password: import.meta.env.VITE_BLUE_SKY_KEY,
   });
 };
 
-export const getProfile = async (actor: string): Promise<ResponseError> => {
+export const getProfile = async (actor: string): Promise<BskyResponse> => {
   try {
-    const response = await agent.getProfile({ actor });
-    return { response, error: null };
+    const response = await bskyAgent.getProfile({ actor });
+    return [response, null];
   } catch {
-    return { response: null, error: "Something went wrong getting profile." };
+    return [null, "Something went wrong getting profile."];
+  }
+};
+
+export const generateRoast = async (actor: string): Promise<GenResponse> => {
+  try {
+    const [bskyResponse, bskyError] = await getProfile(actor);
+
+    if (!bskyResponse || bskyError) {
+      return [null, bskyError];
+    }
+
+    const bskyProfile = bskyResponse.data;
+    console.log("profile", bskyProfile);
+
+    const prompt = `Tell me a roast about a BlueSky user named ${
+      bskyProfile.handle
+    } who joined ${bskyProfile.createdAt} has ${
+      bskyProfile.followersCount || 0
+    } followers.`;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+
+    return [response, null];
+  } catch {
+    return [null, "Something went wrong generating roast"];
   }
 };
